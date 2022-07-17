b9e6300932a82aab73fe466dc09df8d272a6fe25
Виталий Чухляев <vitaliy69@mail.ru>
2022-07-17 21:45:23 +0300

    Completed task number two

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -11,9 +11,19 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
+        self.courses_attached = []
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_lec(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -22,18 +32,9 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
 
-
-class Lecturer(Mentor):
-    pass
+class Lecturer(Mentor, Student):
+    grades = {}
 
 
 class Reviewer(Mentor):
@@ -50,11 +51,19 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+# print(best_student.grades)
+
+# Task-2
+cool_lecturer = Lecturer('Vovan', 'Tolyan')
+cool_lecturer.courses_attached += ['Python']
+
+cool_lecturer.rate_lec(cool_lecturer, 'Python', 10)
+cool_lecturer.rate_lec(cool_lecturer, 'Python', 10)
+print(cool_lecturer.name, cool_lecturer.grades)
