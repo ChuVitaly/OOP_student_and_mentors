3901a1fd4b382216b9ce5eaab3e7369811af7b8d
Виталий Чухляев <vitaliy69@mail.ru>
2022-07-17 14:52:55 +0300

    Теперь только Reviewer могут выставлять студентам оценки за домашние задания.

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -37,7 +37,14 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
